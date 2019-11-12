cbe16982496840541ece74a14c681f83feb65143
nitongxiong <2630832857@qq.com>
2019-11-12 15:09:14 +0800

    源码

---
+++ articles_md/Vue源码/vue源代码/vue-example/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <div>{{msg}}</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return {
+      msg: "Hello World"
+    }
+  }
+}
+</script>
